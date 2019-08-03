8c82d2d3cea1e0abc767a60c01a7ca1e1ddf392a
Aaron Turner <aaron.turner64@hotmail.co.uk>
2019-08-03 18:00:34 +0100

    Added upload feature.

--- dotDefender.go
+++ dotDefender.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "os"
-    "os/exec"
-    "strconv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strconv"
 )
 
 // initialize variables
@@ -14,47 +15,47 @@ import (
 // Files struct which contains
 // an array of files
 type Files struct {
-    Files []File `json:"files"`
+	Files []File `json:"files"`
 }
 
 // Files struct which contains a path,
 // owner, group and chmod
 type File struct {
-    Path   string `json:"path"`
-    Owner  string `json:"owner"`
-    Group  string `json:"group"`
-    Chmod  string `json:"chmod"`
+	Path  string `json:"path"`
+	Owner string `json:"owner"`
+	Group string `json:"group"`
+	Chmod string `json:"chmod"`
 }
 
 // check function for checking for errors
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 // download function for getting data from
 // lastpass and writing to file
 func download(chmod string, path string) {
 
-     // convert chmod string to base8 for os.FileMode
-    out1, err := strconv.ParseInt(chmod, 8, 32)
-    check(err)
-    perm := os.FileMode(out1)
+	// convert chmod string to base8 for os.FileMode
+	out1, err := strconv.ParseInt(chmod, 8, 32)
+	check(err)
+	perm := os.FileMode(out1)
 
-    // set command and args
-    command := "lpass"
-    args := []string{"show", path, "--notes"}
+	// set command and args
+	command := "lpass"
+	args := []string{"show", path, "--notes"}
 
-    //run command
-    cmd := exec.Command(command, args...)
-    out2, err := cmd.CombinedOutput()
-    check(err)
+	//run command
+	cmd := exec.Command(command, args...)
+	out2, err := cmd.CombinedOutput()
+	check(err)
 
-    // write output
-    content := []byte(out2)
-    err = ioutil.WriteFile(path, content, perm)
-    check(err)
+	// write output
+	content := []byte(out2)
+	err = ioutil.WriteFile(path, content, perm)
+	check(err)
 
 }
 
@@ -62,79 +63,84 @@ func download(chmod string, path string) {
 // lastpass
 func upload(path string) {
 
-   // set command and args
-   command := "lpass"
-   args := []string{"edit", path, "--non-interactive", "--notes"}
+	// read file
+	file, err := ioutil.ReadFile(path)
+	check(err)
 
-   //run command
-   cmd := exec.Command(command, args...)
-   out, err := cmd.CombinedOutput()
-   check(err)
+	// set command and args
+	command := "lpass"
+	args := []string{"edit", path, "--non-interactive", "--notes"}
 
-    // print output
-    fmt.Println(out)
+	//run command
+	cmd := exec.Command(command, args...)
+	stdin, err := cmd.StdinPipe()
+	check(err)
 
+	err = cmd.Start()
+	check(err)
+
+	_, err = io.WriteString(stdin, string(file))
+	check(err)
 }
 
 // chown function for changing owner / group
 // of a file
 func chown(owner string, group string, path string) {
 
-    // combine owner and group
-    ownergroup := fmt.Sprint(owner +":"+ group)
+	// combine owner and group
+	ownergroup := fmt.Sprint(owner + ":" + group)
 
-    // set command and args
-    command := "chown"
-    args := []string{ownergroup, path}
+	// set command and args
+	command := "chown"
+	args := []string{ownergroup, path}
 
-    //run command
-    cmd := exec.Command(command, args...)
-    out, err := cmd.CombinedOutput()
-    check(err)
+	//run command
+	cmd := exec.Command(command, args...)
+	out, err := cmd.CombinedOutput()
+	check(err)
 
-    // print output
-    fmt.Println(out)
+	// print output
+	fmt.Println(out)
 
 }
 
 func main() {
 
-    // Open our jsonFile
-    jsonFile, err := os.Open("files.json")
-    check(err)
+	// Open our jsonFile
+	jsonFile, err := os.Open("files.json")
+	check(err)
 
-    // defer the closing of our jsonFile so that we can parse it later on
-    defer jsonFile.Close()
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
 
-    // read our opened xmlFile as a byte array.
-    byteValue, _ := ioutil.ReadAll(jsonFile)
+	// read our opened xmlFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-    // we initialize our Files array
-    var files Files
+	// we initialize our Files array
+	var files Files
 
-    // we unmarshal our byteArray which contains our
-    // jsonFile's content into 'files' which we defined above
-    json.Unmarshal(byteValue, &files)
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'files' which we defined above
+	json.Unmarshal(byteValue, &files)
 
-    // we iterate through every file within our files array and
-    // print out the file path, owner, group and chmod
-    for i := 0; i < len(files.Files); i++ {
-        fmt.Println("Loop Iteration: ", i)
-        fmt.Println("Path: " + files.Files[i].Path)
-        fmt.Println("Owner: " + files.Files[i].Owner)
-        fmt.Println("Group: " + files.Files[i].Group)
-        fmt.Println("Chmod: " + files.Files[i].Chmod)
-        fmt.Println("")
+	// we iterate through every file within our files array and
+	// print out the file path, owner, group and chmod
+	for i := 0; i < len(files.Files); i++ {
+		//fmt.Println("Loop Iteration: ", i)
+		fmt.Println("Path: " + files.Files[i].Path)
+		//fmt.Println("Owner: " + files.Files[i].Owner)
+		//fmt.Println("Group: " + files.Files[i].Group)
+		//fmt.Println("Chmod: " + files.Files[i].Chmod)
+		fmt.Println("")
 
-        // call download function
-        //download(files.Files[i].Chmod, files.Files[i].Path)
+		// call download function
+		//download(files.Files[i].Chmod, files.Files[i].Path)
 
+		// call upload function
+		upload(files.Files[i].Path)
 
-        // call upload function
-        //upload(files.Files[i].Path)
+		// call backup function
+		//backup(backuptype, files.Files[i].Path)
 
-        // call backup function
-        //backup(backuptype, files.Files[i].Path)
-
-    }
+	}
 }
