3693635a3393d2bdaa9bf83191599979239fadc4
Aaron Turner <aaron.turner64@hotmail.co.uk>
2019-08-04 15:06:48 +0100

    tidy up and added flags.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
+    "flag"
 )
 
 // initialize variables
@@ -21,71 +22,78 @@ type Files struct {
 // Files struct which contains a path,
 // owner, group and chmod
 type File struct {
-	Path  string `json:"path"`
-	Owner string `json:"owner"`
-	Group string `json:"group"`
-	Chmod string `json:"chmod"`
+	Name      string `json:"name"`
+	Lpass_id  string `json:"lpass_id"`
+	Path      string `json:"path"`
+	Owner     string `json:"owner"`
+	Group     string `json:"group"`
+	Chmod     string `json:"chmod"`
 }
 
 // check function for checking for errors
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+func check(msg string, err error) {
+	if err != nil {
+	    fmt.Println(msg)
+        panic(err)
+    }
 }
 
 // download function for getting data from
 // lastpass and writing to file
-func download(chmod string, path string) {
+func download(chmod string, id string, path string) {
 
 	// convert chmod string to base8 for os.FileMode
 	out1, err := strconv.ParseInt(chmod, 8, 32)
-	check(err)
+    check("Error: Can't convert chmod string.", err)
 	perm := os.FileMode(out1)
 
 	// set command and args
 	command := "lpass"
-	args := []string{"show", path, "--notes"}
+	args := []string{"show", id, "--notes"}
 
 	//run command
 	cmd := exec.Command(command, args...)
 	out2, err := cmd.CombinedOutput()
-	check(err)
+    check("Error: Can't run lpass command to download data.", err)
 
 	// write output
 	content := []byte(out2)
 	err = ioutil.WriteFile(path, content, perm)
-	check(err)
+    check("Error: Can't write to file.", err)
 
 }
 
 // upload function for sending data to
 // lastpass
-func upload(path string) {
+func upload(path string, id string) {
 
 	// read file
 	file, err := ioutil.ReadFile(path)
-	check(err)
+    check("Error: Can't read file.", err)
 
 	// set command and args
 	command := "lpass"
-	args := []string{"edit", path, "--non-interactive", "--notes"}
+	args := []string{"edit", id, "--non-interactive", "--notes"}
 
 	//run command
 	cmd := exec.Command(command, args...)
 	stdin, err := cmd.StdinPipe()
-	check(err)
+    check("Error: Can't pipe data to lpass command to upload data.", err)
 
 	err = cmd.Start()
-	check(err)
+    check("Error: Can't run lpass command to upload data.", err)
 
 	_, err = io.WriteString(stdin, string(file))
-	check(err)
+	check("", err)
 }
 
 // chown function for changing owner / group
 // of a file
 func chown(owner string, group string, path string) {
+
+	// test file
+	_, err := os.Stat(path)
+    check("Error: File doesn't exit.", err)
 
     // combine owner & group
     ownergroup := fmt.Sprint(owner + ":" + group)
@@ -96,8 +104,8 @@ func chown(owner string, group string, path string) {
 
 	//run command
 	cmd := exec.Command(command, args...)
-    err := cmd.Start()
-	check(err)
+    err = cmd.Start()
+    check("Error: Can't run chown command on file.", err)
 
 }
 
@@ -107,23 +115,28 @@ func chmod(chmod string, path string) {
 
 	// test file
 	_, err := os.Stat(path)
-	check(err)
+    check("Error: File doesn't exit.", err)
 
 	// convert chmod string to base8 for os.FileMode
 	out1, err := strconv.ParseInt(chmod, 8, 32)
-	check(err)
+    check("Error: Can't convert chmod string.", err)
 	perm := os.FileMode(out1)
 
 	err = os.Chmod(path, perm)
-	check(err)
+    check("Error: Can't run chmod command on file.", err)
 
 }
 
 func main() {
 
+    is_download := flag.Bool("download", false, "a bool")
+    is_upload := flag.Bool("upload", false, "a bool")
+
+    flag.Parse()
+
 	// Open our jsonFile
 	jsonFile, err := os.Open("files.json")
-	check(err)
+    check("Error: Can't read files.json.", err)
 
 	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
@@ -141,23 +154,19 @@ func main() {
 	// we iterate through every file within our files array and
 	// print out the file path, owner, group and chmod
 	for i := 0; i < len(files.Files); i++ {
-		//fmt.Println("Loop Iteration: ", i)
-		fmt.Println("Path: " + files.Files[i].Path)
-		//fmt.Println("Owner: " + files.Files[i].Owner)
-		//fmt.Println("Group: " + files.Files[i].Group)
-		//fmt.Println("Chmod: " + files.Files[i].Chmod)
-		fmt.Println("")
 
 		// call download function
-		download(files.Files[i].Chmod, files.Files[i].Path)
-		chown(files.Files[i].Owner, files.Files[i].Group, files.Files[i].Path)
-		chmod(files.Files[i].Chmod, files.Files[i].Path)
+        if *is_download == true {
+            download(files.Files[i].Chmod, files.Files[i].Lpass_id, files.Files[i].Path)
+		    chown(files.Files[i].Owner, files.Files[i].Group, files.Files[i].Path)
+		    chmod(files.Files[i].Chmod, files.Files[i].Path)
+        }
 
 		// call upload function
-		//upload(files.Files[i].Path)
-
-		// call backup function
-		//backup(backuptype, files.Files[i].Path)
+        if *is_upload == true {
+			upload(files.Files[i].Path, files.Files[i].Lpass_id)
+        }
 
 	}
+
 }
